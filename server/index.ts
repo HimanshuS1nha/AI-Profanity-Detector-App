d37e03956a2b61cdad5b5322aca875eecaa6296f
Himanshu Sinha <ashusinha1904@gmail.com>
2024-08-17 22:05:36 +0530

    Validation and error hadling added

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { z, ZodError } from "zod";
 
 const app = new Hono();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
@@ -10,15 +11,32 @@ app.get("/", (ctx) => {
 });
 
 app.post("/api/detect", async (ctx) => {
-  const { prompt } = await ctx.req.json();
+  try {
+    const data = await ctx.req.json();
 
-  const res = await model.generateContent(
-    `You are a profanity detection model. You have to give the following text a score from 0 to 1 where 1 means that you are absolutely sure that the text is profane and 0 means that you are absolutely sure that the text is not profane. Only give the score as response and nothing else. The text is : ${prompt}`
-  );
+    const validator = z.object({
+      text: z
+        .string({ required_error: "Text is required" })
+        .trim()
+        .min(1, { message: "Text is required" }),
+    });
 
-  const score = res.response.text();
+    const { text } = await validator.parseAsync(data);
 
-  return ctx.json({ score });
+    const res = await model.generateContent(
+      `You are a profanity detection model. You have to give the following text a score from 0 to 1 where 1 means that you are absolutely sure that the text is profane and 0 means that you are absolutely sure that the text is not profane. Only give the score as response and nothing else. The text is : ${text}`
+    );
+
+    const score = res.response.text();
+
+    return ctx.json({ score });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return ctx.json({ error: error.errors[0].message });
+    } else {
+      return ctx.json({ error: "Some error occured. Please try again later!" });
+    }
+  }
 });
 
 export default app;
